6963f5fe6e1d20b523594da0ce1951ffa2aae1fd
YellowBabao <167033787+YellowBabao@users.noreply.github.com>
2024-04-14 14:05:51 -0700

    Add files via upload

---
+++ Lab7Code.java
@@ -0,0 +1,15 @@
+public class Lab7Code {
+    public static void main(String[] args) {
+    
+    int x = 0;
+    int y = 0;
+    
+    if (x==y) {
+		x = 2 * x;
+		} else {
+			x = x + (2 * y);
+		}
+	}
+}
+
+
